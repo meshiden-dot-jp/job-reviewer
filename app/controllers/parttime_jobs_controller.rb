e8fc4d9a8d3f88f2a641bb75d947ae33fbd5a3a0
Yuto Iida <meganenasi@gmail.com>
2025-07-14 16:14:47 +0900

    update

--- app/controllers/parttime_jobs_controller.rb
+++ app/controllers/parttime_jobs_controller.rb
@@ -1,5 +1,6 @@
 class ParttimeJobsController < ApplicationController
   before_action :set_parttime_job, only: %i[ show edit update destroy ]
+  before_action :require_login, only: %i[create edit update destroy]
 
   # GET /parttime_jobs or /parttime_jobs.json
   def index
@@ -19,6 +20,7 @@ class ParttimeJobsController < ApplicationController
   # GET /parttime_jobs/new
   def new
     @parttime_job = ParttimeJob.new
+    @parttime_job.category = params[:category] if params[:category].present?
   end
 
   # GET /parttime_jobs/1/edit
@@ -31,7 +33,7 @@ class ParttimeJobsController < ApplicationController
 
     respond_to do |format|
       if @parttime_job.save
-        format.html { redirect_to @parttime_job, notice: "Parttime job was successfully created." }
+        format.html { redirect_to @parttime_job, notice: "シゴト作成成功" }
         format.json { render :show, status: :created, location: @parttime_job }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -44,7 +46,7 @@ class ParttimeJobsController < ApplicationController
   def update
     respond_to do |format|
       if @parttime_job.update(parttime_job_params)
-        format.html { redirect_to @parttime_job, notice: "Parttime job was successfully updated." }
+        format.html { redirect_to @parttime_job, notice: "シゴト更新成功" }
         format.json { render :show, status: :ok, location: @parttime_job }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -58,7 +60,7 @@ class ParttimeJobsController < ApplicationController
     @parttime_job.destroy!
 
     respond_to do |format|
-      format.html { redirect_to parttime_jobs_path, status: :see_other, notice: "Parttime job was successfully destroyed." }
+      format.html { redirect_to parttime_jobs_path, status: :see_other, notice: "シゴト削除成功" }
       format.json { head :no_content }
     end
   end
@@ -72,4 +74,10 @@ class ParttimeJobsController < ApplicationController
     def parttime_job_params
       params.require(:parttime_job).permit(:name, :category)
     end
+
+    def require_login
+      unless logged_in?
+        redirect_to login_path, alert: "ログインするとおシゴトを追加できます。"
+      end
+  end
 end
